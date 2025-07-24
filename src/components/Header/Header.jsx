ba8234600a7b295224ba20ecaf8d86d53b5a3397
JurisNexaIt <shubham.developerjuris@gmail.com>
2025-07-24 14:31:19 +0530

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
 import "./Header.css";
 
 import Navbar from "../Navbar/Navbar";
-import TextBox from "../TextBox/TextBox";
+
 
 const Header = () => {
   return (
     <section className="header">
       <Navbar />
-      <TextBox />
+      
     </section>
   );
 };
